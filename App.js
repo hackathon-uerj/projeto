8a414602b1ee3a24b9b3fefeabab98ed38d08494
almelo <algacyr.junior@gmail.com>
2022-11-05 12:31:04 -0300

    Add home screen and refactor files

--- App.js
+++ App.js
@@ -3,12 +3,17 @@ import { ImageBackground, StyleSheet, Text, View, TouchableOpacity, Image, Alert
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
 
-const background = './assets/Marketing.jpg';
-const play = './assets/Play.png'
-const feed = './assets/Feed.jpg'
-const snack = './assets/Snack.png'
+const home = './assets/home-bg.jpg';
+const kids = './assets/kids-bg.jpg';
+const feedGame = './assets/feed-game-bg.jpg'
+
+const buttonPlay = './assets/button-play.png'
+const snack = './assets/button-snack.png'
+const buttonKids = './assets/button-kids.png';
+const buttonParents = './assets/button-parents.png';
+
+const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
   return (
@@ -16,8 +21,10 @@ const MyStack = () => {
       <Stack.Navigator screenOptions={{
         headerShown: false,
       }}
+      initialRouteName="Home"
       >
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="KidsArea" component={KidsArea} />
         <Stack.Screen name="Feed" component={Feed} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -33,22 +40,43 @@ const App = () => {
 const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require(background)} resizeMode="cover" style={styles.image}>
+      <ImageBackground source={require(home)} resizeMode="cover" style={styles.image}>
       </ImageBackground>
       <TouchableOpacity
-        style={styles.play}
-        onPress={() => navigation.navigate('Feed')}
+        style={styles.buttonKids}
+        onPress={() => navigation.navigate('KidsArea')}
       >
-          <Image source={require(play)}/>
+          <Image source={require(buttonKids)}/>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonParents}
+        onPress={() => navigation.navigate('ParentsArea')}
+      >
+          <Image source={require(buttonParents)}/>
       </TouchableOpacity>
     </View>
   );
 };
 
-const Feed = ({ navigation }) => {
+const KidsArea = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require(feed)} resizeMode="cover" style={styles.image}>
+      <ImageBackground source={require(kids)} resizeMode="cover" style={styles.image}>
+      </ImageBackground>
+      <TouchableOpacity
+        style={styles.play}
+        onPress={() => navigation.navigate('Feed')}
+      >
+          <Image source={require(buttonPlay)}/>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Feed = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require(feedGame)} resizeMode="cover" style={styles.image}>
       </ImageBackground>
       <TouchableOpacity
         style={styles.snack}
@@ -89,6 +117,16 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 545,
     left: 275
+  },
+  buttonKids: {
+    position: "absolute",
+    top: 550,
+    left: 20
+  },
+  buttonParents: {
+    position: "absolute",
+    top: 550,
+    right: 20
   }
 });
 
